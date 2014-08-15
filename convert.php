d85821dbbba3488c8d249725b12e666c0b5e5e13
Honza Novák <unumin@gmail.com>
2014-08-15 23:40:59 +0200

    allows converting code to old syntax

--- convert.php
+++ convert.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Converts array() to [] used by PHP 5.4
+ * Converts between array() and [] used by PHP 5.4
  *
  * @author     David Grudl (http://davidgrudl.com)
  */
@@ -11,11 +11,14 @@ Convertor for PHP 5.4 arrays
 ----------------------------
 ';
 
-if (!isset($_SERVER['argv'][1])) {
-	die("Usage: {$_SERVER['argv'][0]} <directory>");
+$args = $_SERVER['argv'];
+$toOldSyntax = (isset($args[1]) && in_array($args[1], array('-r', '--reverse')));
+
+if (!isset($args[1]) || ($toOldSyntax && !isset($args[2]))) {
+	die("Usage: {$args[0]} [-r|--reverse] <directory>\n");
 }
 
-$dir = $_SERVER['argv'][1];
+$dir = $toOldSyntax ? $args[2] : $args[1];
 if (!is_dir($dir)) {
 	echo "Directory $dir not found.\n";
 	die(1);
@@ -27,7 +30,11 @@ foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir)) as 
 	}
 	echo $file;
 	$orig = file_get_contents($file);
-	$res = convertCode($orig);
+	if (!$toOldSyntax) {
+		$res = convertArraysToSquareBrackets($orig);
+	} else {
+		$res = convertSquareBracketsToArrays($orig);
+	}
 	if ($orig !== $res) {
 		echo " - converted";
 		file_put_contents($file, $res);
@@ -37,11 +44,11 @@ foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir)) as 
 
 
 /**
- * Converts PHP code
+ * Converts array() to []
  * @param  string
  * @return string
  */
-function convertCode($code)
+function convertArraysToSquareBrackets($code)
 {
 	$out = '';
 	$brackets = array();
@@ -65,6 +72,33 @@ function convertCode($code)
 				$brackets[] = TRUE;
 				$token = '[';
 			}
+		}
+		$out .= is_array($token) ? $token[1] : $token;
+	}
+	return $out;
+}
+
+/**
+ * Converts [] to array()
+ * @param  string
+ * @return string
+ * @author Honza Novák (http://honzanovak.com)
+ */
+function convertSquareBracketsToArrays($code)
+{
+	$out = '';
+	$brackets = array();
+	$ignoreBracket = FALSE;
+	foreach (token_get_all($code) as $token) {
+		if ($token === '[') {
+			$brackets[] = !$ignoreBracket;
+			$token = $ignoreBracket ? '[' : 'array(';
+		} elseif ($token == ']'){
+			$token = array_pop($brackets) ? ')' : ']';
+		}
+		if (!is_array($token) || $token[0] !== T_WHITESPACE) {
+			$ignoreBracket = (in_array($token, array(')', ']', '}'))
+				|| (is_array($token) && in_array($token[0], array(T_VARIABLE, T_STRING, T_STRING_VARNAME))));
 		}
 		$out .= is_array($token) ? $token[1] : $token;
 	}
